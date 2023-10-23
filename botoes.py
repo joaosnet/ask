4fddc6f30b0c84304358953099157be1d7d5eff4
joaosnet <87316339+joaosnet@users.noreply.github.com>
2023-10-23 11:51:19 -0300

    Mais wigets

--- botoes.py
+++ botoes.py
@@ -1,6 +1,12 @@
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.button import ButtonBehavior
+from kivy.properties import StringProperty
+from kivymd.uix.relativelayout import MDRelativeLayout
+
+class ClickableTextFieldRound(MDRelativeLayout):
+    text = StringProperty()
+    hint_text = StringProperty()
 
 class ImageButton(ButtonBehavior, Image):
     pass
--- hot_reload_tela.py
+++ hot_reload_tela.py
@@ -1,14 +1,17 @@
 from kivymd.tools.hotreload.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
+from botoes import *
+from telas import *
 
 Window.size = (360, 800) # tamanho da janela do aplicativo
 
+
 class HotReload(MDApp):
-    KV_FILES = ['kv/loginpage.kv']
+    KV_FILES = ['kv/cadastropage.kv']
     DEBUG = True
     def build_app(self):
-        return Builder.load_file('kv/loginpage.kv')
+        return Builder.load_file('kv/cadastropage.kv')
 
 HotReload().run()
     
