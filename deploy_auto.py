73b0db3ec9f0e86fb7815d472ff904424a1f5bf4
Ubuntu <ubuntu@ip-172-31-68-129.ec2.internal>
2023-11-10 15:34:45 +0000

    Atualizando

--- deploy_auto.py
+++ deploy_auto.py
@@ -1,10 +1,18 @@
 # script para automatizar o deploy do app
 import os
-os.system("ls -l")
 os.system("git pull")
 os.system("buildozer android clean")
-os.system("buildozer android debug deploy run logcat > log.txt")
-os.system("git add .")
-os.system("git commit -m 'Atualizando'")
-os.system("git push")
-os.system("gh release create 0.0.1 '/home/ubuntu/ask/bin/inclusiveway-0.1-arm64-v8a_armeabi-v7a-debug.apk'")
+# apagar o arquivo de log
+if os.path.exists("log.txt"):
+    os.remove("log.txt")
+os.system("nohup buildozer -v android debug > log.txt &")
+# verificar se no arquivo de log tem a palavra "APK"
+while True:
+    if os.path.exists("log.txt") and "APK" in open("log.txt").read():
+        os.system("git add .")
+        os.system("git commit -m 'Atualizando'")
+        os.system("git push")
+        # os.system("gh release create 0.0.9 '/home/ubuntu/ask/bin/inclusiveway-0.1-arm64-v8a_armeabi-v7a-debug.apk'")
+        # interagindo com o terminal para colocar a realease no github
+
+        break
