281581b4a799c6d1dcec5ad7f669bd689c0d6a19
Ubuntu <ubuntu@ip-172-31-68-129.ec2.internal>
2023-11-10 16:01:18 +0000

    Atualizando

--- deploy_auto.py
+++ deploy_auto.py
@@ -6,13 +6,19 @@ os.system("buildozer android clean")
 if os.path.exists("log.txt"):
     os.remove("log.txt")
 os.system("nohup buildozer -v android debug > log.txt &")
+
 # verificar se no arquivo de log tem a palavra "APK"
 while True:
-    if os.path.exists("log.txt") and "APK" in open("log.txt").read():
+    if os.path.exists("log.txt") and "# APK inclusiveway-0.1-arm64-v8a_armeabi-v7a-debug.apk available in the bin directory" in open("log.txt").read():
         os.system("git add .")
         os.system("git commit -m 'Atualizando'")
         os.system("git push")
-        # os.system("gh release create 0.0.9 '/home/ubuntu/ask/bin/inclusiveway-0.1-arm64-v8a_armeabi-v7a-debug.apk'")
+        # os.system("gh release create v0.0.8 '/home/ubuntu/ask/bin/inclusiveway-0.1-arm64-v8a_armeabi-v7a-debug.apk'")
         # interagindo com o terminal para colocar a realease no github
-
+        '''
+        ? Title (optional) v0.0.8
+        ? Release notes Leave blank
+        ? Is this a prerelease? Yes
+        ? Submit? Publish release
+        '''
         break
