8458dd97f53a513b1ae9d26c6d7b5212efabdb79
João Natividade <87316339+joaosnet@users.noreply.github.com>
2023-11-21 18:18:48 +0000

    correcoes

--- myredis.py
+++ myredis.py
@@ -1,12 +1,19 @@
-from redis.cluster import RedisCluster
+import redis
 
-rc = RedisCluster(host='master.inclusiveway-db.shrvtq.use1.cache.amazonaws.com', port=6379)
+client = redis.Redis.from_url('redis://3.88.32.1:6379')
+print(client.ping())
 
-print(rc.get_nodes())
+print(client.get('x'))
+
+print(client.get_encoder())
+# from redis.cluster import RedisCluster
+# rc = RedisCluster(host='inwaydb2.shrvtq.ng.0001.use1.cache.amazonaws.com', port=6379)
+
+# print(rc.get_nodes())
 # [[host=127.0.0.1,port=16379,name=127.0.0.1:16379,server_type=primary,redis_connection=Redis<ConnectionPool<Connection<host=127.0.0.1,port=16379,db=0>>>], ...
 
-rc.set('foo', 'bar')
+# rc.set('foo', 'bar')
 # True
 
-rc.get('foo')
+# rc.get('foo')
 # b'bar'
