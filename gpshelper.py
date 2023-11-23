3b1b6edc4c7ffe0944b8c8bc752944682ff2cfeb
joaosnet <87316339+joaosnet@users.noreply.github.com>
2023-11-23 08:16:41 -0300

    Update GPS start parameters and add recentralisar button

--- gpshelper.py
+++ gpshelper.py
@@ -30,7 +30,7 @@ class GpsHelper():
                     from plyer import gps
                     gps.configure(on_location=self.update_blinker_position,
                                 on_status=self.on_auth_status)
-                    gps.start(minTime=500, minDistance=0)
+                    gps.start(minTime=100, minDistance=1)
                 else:
                     print("Permissão de GPS negada")
             request_permissions([Permission.ACCESS_COARSE_LOCATION,
